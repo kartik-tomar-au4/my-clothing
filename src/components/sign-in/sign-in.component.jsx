9307f056fc75136ea017f9d62af6a107d99486d3
kartik-tomar-vst-au4 <kartik9896@gmail.com>
2020-02-22 06:20:24 +0530

    sign in functionality added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sign-in.style.scss';
 
-import { signInWithGoogle } from '../../../src/firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../../src/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
@@ -15,9 +15,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password: ''})
+        
+        const { email, password } = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email: '', password: ''})
+        } catch (error) {
+            console.error(error)
+        }
     } 
 
     handelChange = event => {
